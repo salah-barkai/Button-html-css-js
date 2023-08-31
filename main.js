dfdaf69c5029b84fba30b33c0464fc29461c9632
saladine ahmat barkai <93881972+salah-barkai@users.noreply.github.com>
2023-08-31 20:26:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var button = document.getElementById('btn');
+var tit = document.getElementById('titre');
+
+button.addEventListener('click',() =>{
+    tit.innerHTML='Hello, Bienvenue chez VST...!';
+    tit.style.color= "orangered";
+})
+
